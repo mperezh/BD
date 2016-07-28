435e12ec6b0657019ec459d7097436874d613ba6
Manuel Pérez <manuelph14@gmail.com>
2016-07-28 03:40:41 +0000

    about view :pencil2:

--- home/urls.py
+++ home/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', view=views.index, name='home'),
+    url(r'^home/$', view=views.index, name='home'),
+    url(regex=r'^about/$', view=views.AboutView.as_view(), name='about'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views import generic
 
 # Create your views here.
 def index(request):
     return render(request, 'home/index.html')
+
+class AboutView(generic.View):
+    
+    def get(self, request, *args, **kwargs):
+        return render(request, 'home/about.html')
